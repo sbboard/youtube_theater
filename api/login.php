c3de9366afb315af2228c9e27d27f8c8f3c0aff6
Colin Buffum <colin.buffum@gmail.com>
2018-10-11 01:11:20 -0600

    login mechanic works

---
+++ api/login.php
@@ -0,0 +1,27 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
+require_once("includes/commonvars.php");
+
+$error = null;
+$password = $_POST['password'];
+$username = clean($_POST['username']);
+$savedpass = null;
+$foundUsername = false;
+
+$result = mysqli_query($con, "SELECT * FROM $memberTable WHERE user='$username'");
+while($row = mysqli_fetch_assoc($result)){
+    $foundUsername = true;
+    $savedpass = $row['password'];
+}
+if($foundUsername){
+    if(!password_verify($password,$savedpass)){
+        $error = "Password incorrect";
+    }
+}
+else{
+    $error = "Username not found";
+}
+mysqli_close($con);
+echo $error;
+?>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,8 +6,9 @@
             <input type='text' required v-model='loginName'/>
             Password:{{this.$store.state.username}}
             <input type='password' v-model='loginPass' required/>
-            <button @click="submitRegister()">Send</button>
+            <button @click="submitLogin()">Send</button>
             </form>
+            {{loginError}}
         </div>
         <div v-else>
             Hello {{this.$store.state.username}}!
@@ -16,12 +17,41 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
     name: 'login',
     data(){
         return{
             loginPass:"",
-            loginName:""
+            loginName:"",
+            loginError:""
+        }
+    },
+    methods:{
+        submitLogin(){
+            const params = new URLSearchParams();
+            params.append('username', this.loginName);
+            params.append('password', this.loginPass);
+            var headers = {
+                "Access-Control-Allow-Origin" : "*",
+                "Access-Control-Allow-Methods" : "GET,POST,PUT,DELETE,OPTIONS",
+                "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
+            }
+            axios.post('http://localhost/youtube_theater/api/login.php',params,headers)
+            .then(response => {
+                console.log(response.data);
+                if(response.data==""){
+                    this.$store.state.username=this.loginName
+                }
+                else{
+                    this.loginError=response.data
+                    this.passwordCheck=""
+                    this.password=""
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            })
         }
     }
 }

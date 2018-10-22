f116e693311f7aa9cc1ec8468457bb0768d1460b
Colin Buffum <colin.buffum@gmail.com>
2018-10-22 14:49:20 -0600

    added vue router to project

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import VueYouTubeEmbed from 'vue-youtube-embed'
 
 Vue.use(VueYouTubeEmbed)
-
-Vue.use(Vuetify);
+Vue.use(VueRouter)
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 

08179bb78f5dd3a539ffed552f83ee9d2dc0a0a5
Colin Buffum <colin.buffum@gmail.com>
2018-10-24 15:22:05 -0600

    added youtube vars and listeners for ended

--- src/components/theater/Theater.vue
+++ src/components/theater/Theater.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <youtube :video-id="videoId" player-width="100%" player-height="100%" id="youtTubeVid"></youtube>
+        <youtube :video-id="videoId" @ended="ended" @playing="playing" :player-vars="playerVars" player-width="100%" player-height="100%" id="youtTubeVid"></youtube>
     </div>
 </template>
 
@@ -9,11 +9,29 @@ export default {
     name: "theater",
     data(){
         return{
-            videoId: "02gys42cYmk"
+            videoId: "",
+            playerVars: {
+                'autoplay': 1,
+                'controls': 0, 
+                'disablekb': 1,
+                'fs': 1,
+                'modestbranding': 1
+            }
         }
     },
     created(){
-            this.videoId= "02gys42cYmk"
+            this.videoId= "EzKImzjwGyM"
+    },
+    methods: {
+        ready (event) {
+            this.player = event.player
+        },
+        playing() {
+            console.log('\o/ we are watching!!!')
+        },
+        ended(){
+            console.log("it's over")
+        }
     }
 }
 </script>

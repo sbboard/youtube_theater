5438737a5e20e8940e5c04f28d947db412f021ff
Colin Buffum <colin.buffum@gmail.com>
2018-10-31 16:47:03 -0600

    added and reformatted vuelidate to main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+//plugins
 import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
 import VueYouTubeEmbed from 'vue-youtube-embed'
-
+import Vuelidate from 'vuelidate'
+//js files
 import {store} from './store.js';
 import router from './router.js';
+//css
+import 'vuetify/dist/vuetify.min.css'
 
+Vue.use(Vuelidate)
 Vue.use(VueYouTubeEmbed)
 Vue.use(Vuetify)
 

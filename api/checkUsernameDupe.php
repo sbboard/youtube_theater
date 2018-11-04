011ac03d0b8e52f327a024d50cacdc28fe57434d
Colin Buffum <colin.buffum@gmail.com>
2018-11-04 13:44:49 -0700

    finished backend for username dupe

--- api/checkUsernameDupe.php
+++ api/checkUsernameDupe.php
@@ -1,7 +1,22 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: *');
-require_once('includes/commonvars.php'); 
+require_once('includes/commonvars.php');
 
-echo "false";
+$username=$_GET['name'];
+
+$sql="SELECT * FROM $memberTable WHERE user = '$username'";
+
+if ($result=mysqli_query($con,$sql)){
+  // Return the number of rows in result set
+  $rowcount=mysqli_num_rows($result);
+  if($rowcount > 0){
+      echo "false";
+  }
+  else{
+      echo "true";
+  }
+}
+
+mysqli_close($con);
 ?>

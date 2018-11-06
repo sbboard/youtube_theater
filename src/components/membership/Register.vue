cbda188c71258373453bbba988595b671af18729
Colin Buffum <colin.buffum@gmail.com>
2018-11-06 13:14:50 -0700

    register now points to gang fight

--- src/components/membership/Register.vue
+++ src/components/membership/Register.vue
@@ -54,7 +54,7 @@ export default {
                 required,
                 checkSame: val => {
                     if(val.length === 0) return false
-                    return axios.get('http://localhost/youtube_theater/api/checkUsernameDupe.php',
+                    return axios.get('http://gang-fight.com/youtube_theater/api/checkUsernameDupe.php',
                     {
                     params:{
                         name: val
